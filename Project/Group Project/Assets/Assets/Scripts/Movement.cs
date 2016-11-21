e30f6757e7e3e1ab65337432573d3e3f9aa199fc
S177026 <S177026@ucs.ac.uk>
2016-11-21 17:34:56 +0000

    Animation

--- Project/Group Project/Assets/Assets/Scripts/Movement.cs
+++ Project/Group Project/Assets/Assets/Scripts/Movement.cs
@@ -2,16 +2,46 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-
+    Animator anim;
     public float movementSpeed = 2.5f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.D))
+            
+                anim.SetInteger("State", 1);
+
+        if (Input.GetKeyUp(KeyCode.D))
+
+            anim.SetInteger("State", 0);
+
+        if (Input.GetKeyDown(KeyCode.A))
+
+            anim.SetInteger("State", 3);
+
+        if (Input.GetKeyUp(KeyCode.A))
+
+            anim.SetInteger("State", 0);
+
+        if (Input.GetKeyDown(KeyCode.W))
+
+            anim.SetInteger("State", 4);
+
+        if (Input.GetKeyUp(KeyCode.W))
+
+            anim.SetInteger("State", 0);
+    }
+
 
     public void move(int x, int y)
     {
         this.GetComponent<Rigidbody2D>().velocity = new Vector2((movementSpeed * x), (movementSpeed * y));
+       
     }
 }

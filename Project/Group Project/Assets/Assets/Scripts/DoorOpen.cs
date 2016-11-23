b68ee488cdd52b6d6263b5c76033c06b7371a693
John Dorman <John Dorman>
2016-11-23 12:37:04 +0000

    Added the missing unity scripts and artwork to the working project

---
+++ Project/Group Project/Assets/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpen : MonoBehaviour {
+
+	public GameObject door;
+
+	private bool pressed;
+	private SpriteRenderer dSpriteRenderer;
+	private BoxCollider2D dBoxCollider;
+
+	void Awake(){
+		
+
+	}
+
+
+}

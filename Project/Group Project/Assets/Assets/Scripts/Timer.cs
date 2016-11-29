2c6bd163961bcaf39fc188bd021984e2c5904b94
S177026 <S177026@ucs.ac.uk>
2016-11-29 01:09:39 +0000

    GUI
    Added Level Timer
    Debugs to game over

---
+++ Project/Group Project/Assets/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Image cooldown;
+    public bool levelTime;
+    public float waitTime = 30.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelTime == true)
+        {
+            //Reduce fill amount over 30 seconds
+            cooldown.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+
+            if (cooldown.fillAmount == 0f)
+            {
+                Debug.Log("Game Over");
+            }
+        }
+    }
+}
